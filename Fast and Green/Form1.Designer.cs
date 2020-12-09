9395714abf468ba8d83a244fa95fcacb7cf4405c
pradnica <74064194+pradnica@users.noreply.github.com>
2020-12-09 14:09:27 +0100

    Add files via upload

---
+++ Fast and Green/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Fast_and_Green
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtMissed = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fast_and_Green.Properties.Resources.smiec;
+            this.pictureBox1.Location = new System.Drawing.Point(213, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "trash";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Fast_and_Green.Properties.Resources.smiec;
+            this.pictureBox2.Location = new System.Drawing.Point(512, 103);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 62);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "trash";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Fast_and_Green.Properties.Resources.smiec;
+            this.pictureBox3.Location = new System.Drawing.Point(776, 103);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 62);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "trash";
+            // 
+            // player
+            // 
+            this.player.Image = global::Fast_and_Green.Properties.Resources.smietnik_prawo;
+            this.player.Location = new System.Drawing.Point(544, 598);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(127, 82);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(68, 20);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtMissed
+            // 
+            this.txtMissed.AutoSize = true;
+            this.txtMissed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtMissed.Location = new System.Drawing.Point(143, 9);
+            this.txtMissed.Name = "txtMissed";
+            this.txtMissed.Size = new System.Drawing.Size(76, 20);
+            this.txtMissed.TabIndex = 1;
+            this.txtMissed.Text = "Missed: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtMissed);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Fast and Green";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtMissed;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Fast and Green/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fast_and_Green
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight;
+        int speed = 5;
+        int score = 0;
+        int missed = 0;
+
+        Random randX = new Random();
+        Random randY = new Random();
+
+        PictureBox trash_hit = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainGame(object sender, EventArgs e) //event do Timera 
+        {
+            txtScore.Text = "Score: " + score; //wyswietlanie zmiennej score w labelu
+            txtMissed.Text = "Missed: " + missed; //wyswietlanie zmiennej missed w labelu 
+
+            //przesuwanie gracza w zaleznosci od 
+            //player.Left pozwala na okreslenie odleglosci danego elementu od krawedzi kontenerwa, w ktorym element sie znajduje 
+            if (goLeft == true && player.Left > 1)
+            {
+                player.Left -= 35; //przesuwanie gracza o 30 px w lewo z kazdym kliknieciem strzalki
+                player.Image = Properties.Resources.smietnik_lewo; //zmiana obrazu gracza na "lewy"
+            }
+
+
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += 35; //szybkosc, z jaka gracz moze przesuwac obiektem w prawo
+                player.Image = Properties.Resources.smietnik_prawo; //zmiana obrazu gracza na "prawy"
+            }
+
+
+            //obsluga spadania smieci z gory 
+            foreach (Control x in this.Controls)
+            {
+                //jesli kontrolka x jest typu PictureBox i ma tag "smieci"
+                if (x is PictureBox && (string)x.Tag == "trash")
+                {
+                    x.Top += speed; //Top - pobiera odleglosc miedzy gornba krawedzia kontrolki a gorna krawedzia kontenera
+
+
+
+                    if (x.Top + x.Height > this.ClientSize.Height)
+                    {
+                        trash_hit.Image = Properties.Resources.smiec_zbity;
+
+                        //okreslanie parametrow PictureBoxa odpowiadajacemu pominietemu smieciowi 
+                        trash_hit.Location = x.Location;
+                        trash_hit.Height = 60;
+                        trash_hit.Width = 60;
+                        trash_hit.BackColor = Color.Transparent;
+
+                        this.Controls.Add(trash_hit); //w miejscu w ktorym wystapi pomioniety smiec pojawi sie symbolizujacy to obrazek
+
+                        //losowanie polozenia spadjaych jaj wzgledem gornej i lewej krawedzi kontenera 
+                        x.Top = randY.Next(50, 100) * -1;
+                        x.Left = randX.Next(10, this.ClientSize.Width - x.Width);
+
+                        missed += 1;
+                        player.Image = Properties.Resources.smietnik_ominal;
+                    }
+
+                    //co sie stanie gdy gracz dotkanie krawedzi kontrolki, ktora spada 
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        x.Top = randY.Next(50, 100) * -1;
+                        x.Left = randX.Next(10, this.ClientSize.Width - x.Width);
+                        score += 1;
+                    }
+
+                }
+
+            }
+
+            //przyspieszanie predkosci spadajacych elementow jesli zdobedzie sie 10 punktow 
+            if (score > 10)
+            {
+                speed = 12;
+            }
+
+            //koniec gry i wyswietlenie powiadomienia jesli nie zbierze sie 5 smieci 
+            if (missed > 5)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Game over!" + Environment.NewLine + "You scored " + score);
+                RestartGame();
+            }
+            
+        
+
+
+    }
+
+        //funkcja sprawdzajaca, czy wcisnietete sa przyciski odpowiednio strzalki w lewo i strzalki w prawo
+
+        //jesli jest wcisniety dany przycisk to mozna poruszyc sie w dana strone 
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+        }
+
+        //funkcja sprawdzajaca, czy przyciski strzalki w lewo/prawo zostały zwolnone 
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+        }
+
+        private void RestartGame()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "trash")
+                {
+                    x.Top = randY.Next(50, 100) * -1;
+                    x.Left = randX.Next(10, this.ClientSize.Width - x.Width);
+                }
+            }
+
+            player.Left = this.ClientSize.Width / 2; //gracz jest na srodku kontenera 
+            player.Image = Properties.Resources.smietnik_prawo;
+
+            score = 0;
+            missed = 0;
+            speed = 8;
+
+            goLeft = false;
+            goRight = false;
+
+            GameTimer.Start();
+
+        }
+    }
+}
